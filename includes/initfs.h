e51cc3d2015538a22b4d9c40256ca9f846b27bf4
Corwin McKnight <lioneltabre@gmail.com>
2014-01-04 03:39:58 -0800

    Created initfs tools, added directory map, deleted unnecessary things

--- includes/initfs.h
+++ includes/initfs.h
@@ -8,16 +8,22 @@ struct initfs_superblock{
 	byte builder_identity;
 	byte dev_key;
 	byte reserved[7];
-	unsigned int root_mapblock;
+	unsigned int directory_map;
 };
-struct initfs_mapblock {
-	unsigned char magic;
-	unsigned char type;
-	unsigned char name[32];
-	unsigned int data_addr; //O if no data
-	unsigned int data_lenght; //O if no data
-	unsigned int next_block;
-	unsigned short filler; //To make it 48 bytes.
+struct initfs_directory_map
+{
+	unsigned int no_directories;
+	unsigned int directory_entries[127];
+};
 
+
+
+struct initfs_entry {
+	byte magic; //0xF5
+	byte type; // 0 - file, 1 - executable
+	byte name[48];
+	unsigned int data_addr;
+	unsigned int data_lenght;
+	unsigned short filler; //To make it 64 bytes.
 };
 #endif
