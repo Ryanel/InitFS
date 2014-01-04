f0bac5e9166be8f4e5f36836d74f3a6b5235daff
Corwin McKnight <lioneltabre@gmail.com>
2014-01-04 15:07:22 -0800

    Started work on directory.c

---
+++ libinitfs/directory.c
@@ -0,0 +1,31 @@
+const char* getDirectoryLabel(initfs_directory_entry_t entry)
+{
+	return (const char *)entry.name;
+}
+
+unsigned int detectDirectoryAmount(initfs_directory_map_t toDetect)
+{
+	int i=0;
+	unsigned int count = 0;
+	
+	while(i!=(127 - 1))
+	{
+		if(toDetect.directory_entries[i]!=0x00000000)
+		{
+			count++;
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+	return count;
+}
+
+signed int verifyDirectoryAmount(initfs_directory_map_t toDetect)
+{
+	unsigned int directory_map_no=toDetect.no_directories;
+	unsigned int detected_no = detectDirectoryAmount(toDetect);
+	return directory_map_no - detected_no; //Should return 0 if both are the same, a negative number if detected_no is more, positive if detected_no is less
+}
---
+++ libinitfs/includes/libinitfs.h
@@ -0,0 +1,8 @@
+#ifndef LIB_INITFS_H
+#define LIB_INITFS_H
+#include "initfs.h"
+
+//Permissions
+initfs_permissions_t getPermissions(initfs_directory_entry_t entry);
+
+#endif
